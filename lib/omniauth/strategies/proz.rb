8998f4d7b5d18c401351053ddc206be56a3f4c2d
Kevin Dias <diasks2@gmail.com>
2016-04-07 07:07:08 +0900

    Work on options

--- lib/omniauth/strategies/proz.rb
+++ lib/omniauth/strategies/proz.rb
@@ -4,6 +4,8 @@ module OmniAuth
   module Strategies
     class Proz < OmniAuth::Strategies::OAuth2
 
+      DEFAULT_SCOPE = 'user.email'
+
       option :name, :proz
 
       option :client_options, {
@@ -11,6 +13,8 @@ module OmniAuth
         :authorize_url => '/oauth/authorize',
         :token_url => '/oauth/token'
       }
+
+      option :authorize_options, [:scope]
 
       uid { raw_info['uuid'] }
 
@@ -23,18 +27,30 @@ module OmniAuth
       end
 
       def raw_info
-        puts "Get Raw Info"
         @raw_info ||= access_token.get('https://api.proz.com/v2/user').parsed
       end
 
+      def authorize_params
+        super.tap do |params|
+          %w[scope].each do |v|
+            if request.params[v]
+              params[v.to_sym] = request.params[v]
+            end
+          end
+
+          params[:scope] ||= DEFAULT_SCOPE
+        end
+      end
+
       protected
+
       def build_access_token
         params = {
           'client_id' => client.id,
           'client_secret' => client.secret,
           'code' => request.params['code'],
           'grant_type' => 'authorization_code',
-          'redirect_uri' => 'http://localhost:3000/translators/auth/proz/callback'
+          'redirect_uri' => options[:callback_url]
           }.merge(token_params.to_hash(symbolize_keys: true))
         client.get_token(params, deep_symbolize(options.auth_token_params))
       end
